7d042af93d7922f4ab1e405c87ef813ef76e4a90
陈十一 <53396069+mimang11@users.noreply.github.com>
2024-04-11 17:32:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.get('/getUser', (req, res) => {
+    console.log("Just got a request!")
+    res.send('当前没有任何数据!')
+})
 app.listen(process.env.PORT || 3000)
